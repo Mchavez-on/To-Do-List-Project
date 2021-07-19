44ca2e609ad78ba6fb4bf97f3f391804d66b3134
Mariana Chavez <1069756@etusd.org>
2021-07-19 15:28:03 -0700

    "First Commit"

---
+++ To-Do-List Project/ToDo.swift
@@ -0,0 +1,12 @@
+//
+//  ToDo.swift
+//  To-Do-List Project
+//
+//  Created by Mariana Chavez on 7/19/21.
+//
+
+import UIKit
+    class ToDo {
+        var name = ""
+        var important = false
+    }
